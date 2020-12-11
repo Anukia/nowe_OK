3ff38fb35db0ee4d3e21462de1ea769924fbb9d6
Anukia <59363243+Anukia@users.noreply.github.com>
2020-12-11 11:48:02 +0100

    poprawka z wskaźnikami

--- genetyczny.cpp
+++ genetyczny.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int genetyczny(int l_procesorow, int zadania, vector<int> lista)
 {
-	vector<DNA>* stare_pokolenie;
-	vector<DNA>* nowe_pokolenie;
+	vector<DNA*> stare_pokolenie;
+	vector<DNA*> nowe_pokolenie;
 	
 	vector<int> czasy_tmp, rozwiazanie_tmp;
 	zachlannie_1_genetic(l_procesorow, zadania, lista, czasy_tmp, rozwiazanie_tmp);
-	stare_pokolenie->push_back(*new DNA(czasy_tmp, rozwiazanie_tmp));
+	stare_pokolenie.push_back(new DNA(czasy_tmp, rozwiazanie_tmp));
 	czasy_tmp.clear(); rozwiazanie_tmp.clear();
 	zachlannie_2_genetic(l_procesorow, zadania, lista, czasy_tmp, rozwiazanie_tmp);
-	stare_pokolenie->push_back(*new DNA(czasy_tmp, rozwiazanie_tmp));
+	stare_pokolenie.push_back(new DNA(czasy_tmp, rozwiazanie_tmp));
 
 
 	return 0;
